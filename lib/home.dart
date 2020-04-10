321683abda52e338c479c1f5317f19dc54ab14bf
Kauê Murakami <kauetmurakami@gmail.com>
2020-04-10 20:32:21 -0300

    configurando projeto e testando Map normal

---
+++ lib/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mapas e Geolocalização"),
+      ),
+      body: Container(
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.42796133580664, -122.085749655962),
+            zoom: 15
+          ),
+          onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
+  }
+}
